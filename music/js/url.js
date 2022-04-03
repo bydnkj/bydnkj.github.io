3c414dbd877d56185d46478910284e586ceb09b8
gapless <74377275+bydnkj@users.noreply.github.com>
2022-04-04 03:34:51 +0800

    Add files via upload

---
+++ music/js/url.js
@@ -0,0 +1,19 @@
+var	playlist = [{title: "本兮",
+				artist: "轻轻",
+				mp3: "https://sharefs.yun.kugou.com/202103041152/b6661d30c54c42a2d8c0ffa066cbf834/G204/M0B/0F/1F/DA4DAF5fZJiAawBIAD-pI5d2xcc193.mp3",
+				cover: "http://imge.kugou.com/stdmusic/150/20200620/20200620085238245593.jpg",
+			},{title: "侧脸",
+				artist: "于果",
+				mp3: "http://music.163.com/song/media/outer/url?id=534542079.mp3",
+				cover: "https://s1.ax1x.com/2020/08/03/aN8ObD.png",
+			},{title: "无人之岛",             
+				artist: "任然",
+				mp3: "http://music.163.com/song/media/outer/url?id=493735012.mp3",
+				cover: "https://s1.ax1x.com/2020/08/03/aN8ObD.png",
+			},{title: "失眠飞行",
+				artist: "蕾儿",
+				mp3: "http://music.163.com/song/media/outer/url?id=1386055610.mp3",
+				cover: "https://s1.ax1x.com/2020/08/03/aN8ObD.png",
+			}];
+	    var isRotate = true;<!--图片旋转-->
+	    var autoplay = true;<!--自动播放-->
